ab915ab9aec9be7706a3caa8eb0a722e485f76b0
yamap_55 <tetuya120@hotmail.com>
2014-02-01 15:25:51 +0900

    change that.

--- LoginTable.js
+++ LoginTable.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       指定ユーザでログイン
 // @namespace  http://d.hatena.ne.jp/yamap_55/
-// @version    0.5
+// @version    0.6
 // @description  指定ユーザでログインします。
 // @match      http://*/*Login.html
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
@@ -18,27 +18,23 @@ LoginTableFunction.prototype.create = function(args) {
                   id:'idtable'
               }).append($('<tr>').append($('<th>').text('Name'))).css("border","1px solid #ddd").appendTo("body");
     
-    var u = this.userSelector;
-    var p = this.passSelector;
-    var b = this.buttonSelector;
+    var that = this;
     args.forEach(function(arg){
         var name = arg[0]
         var id = arg[1]?arg[1]:name;
         var pass = arg[2]?arg[2]:id;
-        console.log(u)
         t.append($('<tr>',{
             click: function(){
-                $(u).val(id);
-                $(p).val(pass);
-                $(b).trigger("click");
+                $(that.userSelector).val(id);
+                $(that.passSelector).val(pass);
+                $(that.buttonSelector).trigger("click");
             }}).css("cursor","pointer").hover(
-            function(){$(this).css("background-color",'#d8d8d8')},
-            function(){$(this).css("background-color",'#ffffff')}
-        )
-                 .append($('<td>',{
-                     id:name,
-                     text:name,
-                 })))
+            function(){$(this).css("background-color",'#d8d8d8');},
+            function(){$(this).css("background-color",'#ffffff');}
+        ).append($('<td>',{
+            id:name,
+            text:name
+        })))
     });
     $('#idtable tr').each(function(i) {
         if(i != 0) {
@@ -48,13 +44,13 @@ LoginTableFunction.prototype.create = function(args) {
         }
     });
 }
-var user = '#login_user'
-var password = '#login_password'
-var button = '#login_button'
+var user = '#login_user';
+var password = '#login_password';
+var button = '#login_button';
 var func = new LoginTableFunction(user, password, button);
 
 func.create([
     ["test01"],
     ["admin"],
-    ["特殊ユーザ","hoge","huga"],
+    ["特殊ユーザ","hoge","huga"]
 ]);
